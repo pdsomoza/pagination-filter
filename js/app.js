dd0933cd57afffe8713940355bf383445494c051
psdomoza <pdsomoza@gmail.com>
2017-11-28 21:11:44 -0300

    paginator, animation between pages

--- js/app.js
+++ js/app.js
@@ -1,27 +1,40 @@
 (() => {
   const students = document.querySelectorAll('.student-item');
   const len = students.length;
-  const page = 10;
-  const numOfLinks = Math.ceil(len/page);
-  let i;
+  const page = 10, pages = Math.ceil(len/page);
   
-  function loadPage(){
-    for(i = 0; i < page; i++)
-      students[i].style.display = 'block';
+  showStudents();
+  paginator();
+  
+  let links = document.querySelectorAll('.pagination a');  
+  links.forEach((link) => {
+    link.addEventListener('click',(e) => {
+      links.forEach((link) => link.classList.remove('active'));      
+      link.classList.add('active');
+      students.forEach((student) => student.style.display = "" )
+      showStudents(link.dataset.page);
+      e.preventDefault();
+    });
+  });
+
+  function showStudents(to = 1) {
+    let ii = 0;
+    to = page * to;
+    if (to > 10) ii = to - page;
+    if (to > len) to = len;
+
+    for(; ii < to; ii++)
+      students[ii].style.display = 'block';
+  }
+
+  function paginator() {
+    let li = "";
+    for(let ii = 1; ii <= pages; ii++)
+       li += `<li><a href="#" data-page="${ii}">${ii}</a></li>`;
+
+    document.querySelector('.pagination').innerHTML = `<ul>${li}</ul>`;
+    document.querySelectorAll('.pagination a')[0].classList.add('active'); 
   }
   
-  function initPagination(){
-    let links = "";
-    for(i = 1; i < numOfLinks; i++)
-    links += `<li><a href="#">${i}</a></li>`;
-  
-    document.querySelector('.pagination').innerHTML = `<ul>${links}</ul>`;
-    document.querySelectorAll('.pagination li a')[0].classList.add('active'); 
-  }
-
-
-  loadPage();
-  initPagination();
-
 })();
 
